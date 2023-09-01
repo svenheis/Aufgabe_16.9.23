aa013fb8888900fab0cc4e48a14fcc7ff39519b5
svenheis <135041386+svenheis@users.noreply.github.com>
2023-09-01 20:43:44 +0200

    Add files via upload

---
+++ aufgabe für 16.9.23/script.js
@@ -0,0 +1,11 @@
+/* JavaScript umkehren */
+
+/* mit .reverse */
+
+let code = "Javascript";
+
+const rückwärts = code.split("").reverse().join("");
+
+console.log(rückwärts);
+
+/* ohne .reverse */
